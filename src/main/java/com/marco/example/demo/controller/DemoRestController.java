4afcd99a5fae632c4777ac931e84e56181e14d6e
marcochanhk <mc_marcochan@outlook.com>
2021-06-06 21:17:07 +0800

    Update DemoRestController.java

--- src/main/java/com/marco/example/demo/controller/DemoRestController.java
+++ src/main/java/com/marco/example/demo/controller/DemoRestController.java
@@ -1,9 +1,15 @@
 package com.marco.example.demo.controller;
 
+import com.marco.example.demo.dao.UserDao;
+import com.marco.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoRestController {
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/helloWorld")
     public String sayHelloWorld() {
@@ -11,8 +17,8 @@ public class DemoRestController {
     }
 
     @GetMapping("/user")
-    public String getUser() {
-        return "Get user";
+    public UserDao getUser(UserDao user) {
+        return (UserDao) userService.getUser();
     }
 
     @PostMapping("/user")
